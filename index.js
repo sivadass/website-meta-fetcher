da8a17ae33e0b2f8e8a7ef5cba675590df41fe4a
Sivadass N <nsivadass@gmail.com>
2018-06-21 22:51:29 +0530

    Now site description will be fetched from URL

--- index.js
+++ index.js
@@ -3,23 +3,6 @@ const express = require("express");
 const asyncMiddleware = require("./utils/async-middleware");
 const port = process.env.PORT || 3000;
 
-/* (async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://sivadass.in");
-  let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-  let favicon = await page.evaluate(
-    () => document.querySelector('link[rel="shortcut icon"]').href
-  );
-  const pageTitle = await page.title();
-  console.log(pageTitle);
-  console.log(favicon);
-  //await page.screenshot({ path: "example.png" });
-
-  await browser.close();
-})();
- */
-
 const app = express();
 app.set("view engine", "pug");
 app.use(express.static(__dirname + "/static"));
@@ -28,30 +11,6 @@ app.get("/", (req, res) =>
   //res.send("Welcome to Website Meta Fetcher powered by Google's Puppeteer")
   res.render("index", { title: "Hey", message: "Hello there!" })
 );
-// app.get("/api/fetch/", (req, res) => {
-//   const url = req.query.url;
-//   let result = {};
-//   async (result, url) => {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-
-//     await page.goto(url);
-//     //let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-//     let favicon = await page.evaluate(
-//       () => document.querySelector('link[rel="shortcut icon"]').href
-//     );
-//     const pageTitle = await page.title();
-//     console.log(pageTitle);
-//     console.log(favicon);
-//     result = {
-//       title: pageTitle,
-//       favicon: favicon
-//     };
-//     await browser.close();
-//     return result;
-//   };
-//   await res.json(result);
-// });
 
 app.get(
   "/api/fetch/",
@@ -71,11 +30,13 @@ app.get(
     let favicon = await page.evaluate(
       () => document.querySelector('link[rel="shortcut icon"]').href
     );
+    let metaDescription = await page.evaluate(
+      () => document.querySelector('meta[name="description"]').content
+    );
     const pageTitle = await page.title();
-    console.log(pageTitle);
-    console.log(favicon);
     let result = {
       title: pageTitle,
+      metaDescription: metaDescription,
       favicon: favicon
     };
     await browser.close();
